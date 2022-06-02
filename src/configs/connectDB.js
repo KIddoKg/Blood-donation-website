d19dd07f272ef9bb57a0f543c87c81542a9decfb
HuynhNhu1511 <73732151+HuynhNhu1511@users.noreply.github.com>
2022-06-02 16:20:09 +0700

    connection

---
+++ src/configs/connectDB.js
@@ -0,0 +1,20 @@
+// get the client
+// const mysql = require("mysql2");
+import mysql from "mysql2";
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "12345",
+  database: "test",
+});
+
+// simple query
+connection.query(
+  'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
+  function (err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  }
+);
